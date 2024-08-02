bd042fb260b6c0bda496322de4dffe85d82ce9ec
ArenaofDust <149437984+ArenaofDust@users.noreply.github.com>
2024-08-01 22:03:21 -0400

    Added CVForm functionality

---
+++ cv-app/src/components/CVForm.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Form from "./Form";
+import DisplayCV from "./DisplayCV";
+
+const CVForm = () => {
+    const initialInfo = {
+        generalInfo: null
+    };
+
+    const [info, setInfo] = useState(initialInfo);
+
+    //CV cleared of contents, empty according to initialInfo object
+    const clearCV = () => {
+        setInfo(initialInfo);
+    };
+
+    return (
+        <div className="cvForm">
+            <div className="Form">
+                <Form 
+                    onSaveGeneralInfo={(info) => setInfo((prevInfo) => ({...prevInfo, generalInfo: info}))}
+                />
+                <button onClick={clearCV}>Clear Resume</button>
+            </div>
+            <div className="DisplayCV">
+                <DisplayCV
+                    generalInfo={info.generalInfo}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default CVForm;
