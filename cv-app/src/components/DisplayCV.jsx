a63a583894ebb19f5efffc00f46344a1f0f17d25
ArenaofDust <149437984+ArenaofDust@users.noreply.github.com>
2024-08-01 22:03:42 -0400

    Added displaycv functionality

---
+++ cv-app/src/components/DisplayCV.jsx
@@ -0,0 +1,30 @@
+const DisplayCV = ({generalInfo}) => {
+    return (
+        <div className="display">
+            <div className="header">
+                {generalInfo && (
+                    <div>
+                        <h2>{generalInfo.name}</h2>
+                        <div className="personalInfo">
+                            <p className="personalDetails">
+                                <span className="material-symbols-outlined">call</span>{" "}
+                                {generalInfo.phone}
+                            </p>
+                            <p className="personalDetails">
+                                <span className="material-symbols-outlined">mail</span>
+                                {generalInfo.email}
+                            </p>
+                            <p className="personalDetails">
+                                <span className="material-symbols-outlined">location_on</span>{" "}
+                                {generalInfo.location}
+                            </p>
+
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default DisplayCV;
